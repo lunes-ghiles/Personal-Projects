0b14c905637106f31c190a1926c990b2afb30fc2
Lunes Maibeche <lunes.maibeche@mail.utoronto.ca>
2025-07-11 23:41:53 -0400

    Update btop.cpp

--- btop.cpp
+++ btop.cpp
@@ -22,56 +22,84 @@
 // 5. Learn how the how to leverage dynamic memory allocation (isn't that the whole point of C++?)
 
 
-// Function header goes here
-
-void BOPEuroPut(double spot, double exercise, double rfRate, double timeToMaturity, double volatility, int steps) {
-   // Intialize all the variables that we're going to be using 
-    const double timeInterval = timeToMaturity/steps;                                                              // Time interval per step
-    const double upFactor = std::exp(volatility * std::sqrt(timeInterval));                                        // Up factor
-    const double downFactor = 1 / upFactor;                                                                        // Down factor
-    const double discountFactor = std::exp(-rfRate * timeInterval);                                                // Discount factor per step
-    const double probability = (std::exp(rfRate * timeInterval) - downFactor) / (upFactor - downFactor);           // Risk-neutral probability of up move
-   
     // We're going to do some explaining here:
     // This approach is based off of three important optimizations noted by Desmond J Higham
     // when implementing the binomial option pricing model in MATLAB. 
     // 1. We can find the cutoff point past which the option payoff is zero in order to avoid
     //   calculating zero payoffs while also maintaining a smaller array for the terminal option values.
-    // 2. We can use vectorization and binomial expansion to calculate with minimal iteration
+    // 2. We can use binomial expansion to calculate with minimal iteration
     // 3. We can use logarithms to avoid overflow/underflow issues.
 
-
-    // This derives the aforementioned cutoff point, which is the point past which the option payoff becomes zero.
-    double tempVar1 = std::log((exercise * upFactor) / (spot * std::pow(downFactor, steps + 1)));
-    double tempVar2 = std::log(upFactor / downFactor);
-    double rawIndex = tempVar1 / tempVar2;
-
+double BOPEuroPut(double spot, double exercise, double rfRate, double timeToMaturity, double volatility, int steps) {
+   // Intialize all the variables that we're going to be using 
+    const double timeInterval = timeToMaturity/steps;                                                              // Time interval per step
+    const double upFactor = std::exp(volatility * std::sqrt(timeInterval));                                        // Up factor
+    const double downFactor = 1 / upFactor;                                                                        // Down factor
+    const double discountFactor = std::exp(-rfRate * timeInterval);                                                // Discount factor per step
+    const double upProbability = (std::exp(rfRate * timeInterval) - downFactor) / (upFactor - downFactor);         // Risk-neutral probability of up move
+    const double downProbability = 1 - upProbability;                                                               // Risk-neutral probability of down move
+    
+    double rawIndex;
+    // Now IN THEORY, this empty scope should be able to free up memory once the calculation is done WITHOUT 
+    // messing up rawIndex in the process. But at the same time the compiler might be doing this anyways.
+    {
+        double tempVar1 = std::log((exercise * upFactor) / (spot * std::pow(downFactor, steps + 1)));
+        double tempVar2 = std::log(upFactor / downFactor);
+        rawIndex = tempVar1 / tempVar2;
+    }
     // This is changes the previous float estimate to an integer index for the binomial tree.
     int cutoff = static_cast<int>(std::floor(rawIndex));
     // We gotta make sure that the cutoff is within the bounds of the binomial tree
-    if (cutoff < 0) {
+    if (cutoff > (steps + 1)) {
         cutoff = steps + 1;
     }
-    cutoff = std::min(steps + 1, cutoff);
-    z = std::max(1, z);
-
+    if (cutoff < 1){
+        cutoff = 1;
+    }
+    
     // Find possible option value at maturity (based on the binomial tree)
-    std::vector<double> W(z);
+    std::vector<double> payoffs(cutoff, 0.0);
     // I'm not sure if this dangerous because I haven't assigned z as a size
-    for (int i = 0; i < z; ++i) {
-        int d_exp = N - i;
+    for (int i = 0; i < cutoff; ++i) {
+        int d_exp = steps - i;
         int u_exp = i;
-        double term = S * std::pow(d, d_exp) * std::pow(u, u_exp);
-        W[i] = X - term;
+        double term = spot * std::pow(downFactor, d_exp) * std::pow(upFactor, u_exp);
+        payoffs[i] = exercise - term;
+    }
+   
+    // Now we gotta initiate the binomial coefficient calculation vectors
+    double tempVar1;
+    tempVar1 = 0;
+    double tempVar2;
+    tempVar2 = 0;
+    std::vector<double> upLogProbabilities(cutoff, 0.0);
+    std::vector<double> downLogProbabilities(cutoff, 0.0);
+    downLogProbabilities[0] = std::log(downProbability) * steps; // Down probability for 0 up moves
+
+    // I am so proud of this loop - basically smooshed what took 4 vectors in MATLAB into one for loop
+    for (int i = 1; i < cutoff; ++i) {
+        double decreasingSeries = static_cast<double>(steps - i + 1);        // Decreasing series from steps to steps - z + 2
+        double increasingSeries = static_cast<double>(i);                    // Increasing series from 1 to z-1
+        tempVar1 += std::log(decreasingSeries);
+        tempVar2 += std::log(increasingSeries);
+        upLogProbabilities[i] = std::log(upProbability) * increasingSeries;
+        downLogProbabilities[i] = std::log(downProbability) * (decreasingSeries-1);
     }
 
-    double tmp1 = std::accumulate(/* This is where we compute the binomial coefficient */);
+    double tmp1 = tempVar1 - tempVar2; // This is the log of the binomial coefficient
+    double finalValue = 0.0;
+    for(int i = 1; i < cutoff; ++i) {
+        finalValue += payoffs[i] * std::exp(tmp1 + upLogProbabilities[i] + downLogProbabilities[i]);
+    }
+    finalValue = finalValue * discountFactor; // Discount the final value to present value
+    return finalValue;
 }
 
 
+
 int main() {
-    // This is basically just a place holder for now so we can run debugger without the
-   // compiler throwing a fit. SPOILER: It still throws a fit.
-    std::cout << "BOPM calculation is not yet implemented." << '\n';
+    std::cout << BOPEuroPut(65.5, 68, 0.048, 1, 0.325, 3) << std::endl;
+    // Aaaaaand its wrong. 
+    // DEBUGGING TIME
     return 0;
 }
